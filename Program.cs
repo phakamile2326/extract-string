9012f0b647ac68f6acffc91d85bb654d7a5ffc1e
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-17 03:19:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extract_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string inputString = Console.ReadLine();
+            string extractedSubstring = ExtractSubstringBetweenHashSigns(inputString);
+
+            if(string.IsNullOrEmpty(extractedSubstring) )
+            {
+                Console.WriteLine("no substring found between '##' .");
+            }
+            else
+            {
+                Console.WriteLine("Extracted Substring " +  extractedSubstring);
+
+            }
+            Console.ReadLine();
+        }
+        static string ExtractSubstringBetweenHashSigns(string input)
+        {
+            int startIndex = input.IndexOf("##");
+            int endIndex = input.IndexOf("##" , startIndex +2);
+
+            if (startIndex >=0 && endIndex > startIndex) 
+            {
+                startIndex += 2;
+                return input.Substring(startIndex, endIndex - startIndex);
+            }
+            return string.Empty;
+        }
+    }
+}
